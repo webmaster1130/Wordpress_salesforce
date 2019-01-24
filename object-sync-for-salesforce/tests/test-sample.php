0c8beda14a5c66afddc71e1d92267db1d6038db6
webmaster1130 <46073763+webmaster1130@users.noreply.github.com>
2019-01-25 01:40:30 +1000

    Initialize
    Wordpress ,Salesforce Integrate api

---
+++ object-sync-for-salesforce/tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Object_Sync_Salesforce
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
